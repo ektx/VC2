7a4ff737e6e9e8c0b263afcfb91fc3de50d48c33
wenlong <530675800@qq.com>
2020-06-19 13:50:34 +0800

    调整返回格式

--- bin/main.js
+++ bin/main.js
@@ -13,7 +13,8 @@ router.get('/api/doc/:file', async ctx => {
   let html = md.render(str)
 
   html = `<div class="markdown-it-mode">${html}</div>`
-
+  
+  ctx.type = 'json'
   ctx.body = { data: html }
 })
 
@@ -26,22 +27,6 @@ const myPlugin = ({
   app
     .use(router.routes())
     .use(async (ctx, next) => {
-    // You can do pre-processing here - this will be the raw incoming requests
-    // before vite touches it.
-    // console.log('path:', ctx.path)
-    // console.log('response:', ctx.path)
-    if (ctx.path.endsWith('.md')) {
-      // Note vue <style lang="xxx"> are supported by
-      // default as long as the corresponding pre-processor is installed, so this
-      // only applies to <link ref="stylesheet" href="*.scss"> or js imports like
-      // `import '*.scss'`.
-      console.log('pre processing: ', ctx.url)
-      ctx.type = 'json'
-      // ctx.body = 'body { border: 1px solid red }'
-      // let md = new MarkdownIt()
-      // console.log(md.render('# hello'))
-      // ctx.body = { data: md.render('# hello world') }
-    }
 
     // ...wait for vite to do built-in transforms
     await next()
