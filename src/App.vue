22639f301f089c08de57f6bbe99f5c36e7d46716
wenlong <530675800@qq.com>
2020-06-24 14:46:07 +0800

    调整页面

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Home msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
+import Home from './views/home.vue'
 import { compile, h } from 'vue'
 
 export default {
   name: 'App',
-  // components: {
-  //   HelloWorld
-  // },
-  methods: {
-    test () {
-      let render = compile('<HelloWorld msg="Hello Vue 3.0 + Vite" />')
-      // let render = compile('<h1>Hello</h1>')
-      console.log(render)
-      return render
-    }
-  },
-  // render () {
-  //   // return this.test()
-  //   return h(this.test())
-  //   // return h('div', { class: 'custom-renderer' })
-  // }
+  components: { Home }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,11 @@ import App from './App.vue'
 import './assets/reset-1.5.0.css'
 import './assets/theme.less'
 
-import HelloWorld from './components/HelloWorld.vue'
 import VCMarkedIt from './components/VCMarkedIt/index.vue'
 import VCDemoCode from './components/VCDemo/index.vue'
 
 let app = createApp(App)
 
-app.component('HelloWorld', HelloWorld)
 app.component('VCMarkedIt', VCMarkedIt)
 app.component('demo', VCDemoCode)
 app.mount('#app')
