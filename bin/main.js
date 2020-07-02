8c723053dfc4b18f72008497aca990006b83f3b6
wenlong <530675800@qq.com>
2020-07-02 11:10:08 +0800

    添加图标功能

--- bin/main.js
+++ bin/main.js
@@ -2,15 +2,16 @@ const { createServer } = require('vite')
 const Router = require('koa-router')
 const MarkdownIt = require('markdown-it')
 const Prism = require('prismjs')
-const fs = require('fs/promises')
+const fs = require('fs')
 const path = require('path')
 const mdVue = require('./vue')
 
 let router = new Router()
 
+// 获取 doc 中的 markdown 文件
 router.get('/api/doc', async ctx => {
   let file = path.join(__dirname, `../doc/${ctx.request.query.file}`)
-  let str = await fs.readFile(file, { encoding: 'utf-8'})
+  let str = await fs.promises.readFile(file, { encoding: 'utf-8'})
   let md = new MarkdownIt({
     html: true,
     highlight(str, lang) {
@@ -33,6 +34,12 @@ router.get('/api/doc', async ctx => {
   ctx.body = { data: html }
 })
 
+// 获取 icon json
+router.get('/api/icons', async ctx => {
+  let file = path.join(__dirname, '../example/data/iconfont.json')
+  ctx.body= fs.createReadStream(file, {encoding: 'utf8'})
+})
+
 const myPlugin = ({
   root, // 项目目录的绝对地址
   app, // Koa app
@@ -51,14 +58,11 @@ const myPlugin = ({
       // `application/javascript` and <style> are served as `text/css`.
       if (ctx.response.is('js')) {
         console.log('post processing:', ctx.url)
-        // console.log(ctx.body) // can be string or Readable stream
       }
     })
 }
 
 createServer({
-  // root: './example',
-  // base: 'abcd',
   configureServer: [myPlugin],
   alias: {
     vue: 'vue/dist/vue.esm-bundler.js'
--- doc/icon.md
+++ doc/icon.md
@@ -7,3 +7,62 @@
 </template>
 ```
 :::
+
+### 图标集合
+
+::: demo
+```html
+<template>
+  <ul class="icon-list">
+    <li v-for="icon in $icon" :key="icon.icon_id">
+      <i :class="'vc-icon-' + icon.font_class"></i>
+      <span class="icon-name">{{'vc-icon-' + icon.font_class}}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  setup() {
+    let $icon = ref([])
+
+    fetch('/api/icons')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+
+        $icon.value = res.glyphs
+      })
+
+    return {
+      $icon
+    }
+  }
+}
+</script>
+
+<style>
+.icon-list {
+  display: grid;
+  grid-auto-rows: 100px;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+}
+.icon-list li {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.icon-list li i {
+  font-size: 32px;
+  color: #666;
+}
+.icon-list li span {
+  margin: 10px 0 0;
+  font-size: 12px;
+  color: #777;
+  text-align: center;
+}
+</style>
+```
+:::
