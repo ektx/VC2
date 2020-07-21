e8ca2ce4bdb5d01b8e1333eb02672b1e5ff696d4
wenlong <530675800@qq.com>
2020-07-21 14:31:35 +0800

    Update hexPanel.vue

--- packages/components/colorPicker/hexPanel.vue
+++ packages/components/colorPicker/hexPanel.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="vc-color-picker__hex-panel">
-    <input type="text" v-model="hex">
-  </div>
+  <ul class="vc-color-picker__hex-panel color-picker__text-table">
+    <li>
+      <input type="text" v-model="hex" @blur="changeEvt">
+      <p>十六进制</p>
+    </li>
+    <li>
+      <input type="text" v-model="alpha">
+      <p>A</p>
+    </li>
+  </ul>
 </template>
 
 <script>
-import { hex } from './color'
+import { hex, alpha, formatString, updateColorPanel } from './color'
 
 export default {
   setup() {
 
+    function changeEvt(evt) {
+      evt.stopPropagation()
+      
+      formatString(hex.value)
+      updateColorPanel()
+    }
+
     return {
-      hex
+      hex,
+      alpha,
+      changeEvt
     }
   }
 }
