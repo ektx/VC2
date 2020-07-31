f31ec6ab7f09b9e13d77e57549dc92cbb1a0b92b
wenlong <530675800@qq.com>
2020-07-31 16:30:00 +0800

    更新

--- packages/index.js
+++ packages/index.js
@@ -1,40 +1,52 @@
-/* 以下代码自动生成于 Sat Jul 18 2020 10:11:22 GMT+0800 (GMT+08:00) */
+/* 以下代码自动生成于 Fri Jul 31 2020 16:27:50 GMT+0800 (中国标准时间) */
 
 import vcButton from './components/button/index.vue'
-import vcForm from './components/form/index.vue'
 import vcFormItem from './components/formItem/index.vue'
+import vcForm from './components/form/index.vue'
+import vcButtonGroup from './components/buttonGroup/index.vue'
 import vcIcon from './components/icon/index.vue'
-import vcNotification from './components/notification/index.js'
+import vcColorPicker from './components/colorPicker/index.vue'
 import vcInput from './components/input/index.vue'
+import vcLayer from './components/layer/index.vue'
+import vcSelect from './components/select/index.vue'
 import vcMessage from './components/message/index.js'
+import vcNotification from './components/notification/index.js'
 
 const components = [
-    vcButton,
-    vcForm,
-    vcFormItem,
-    vcIcon,
-    vcNotification,
-    vcInput,
-    vcMessage
+  vcButton,
+  vcFormItem,
+  vcForm,
+  vcButtonGroup,
+  vcIcon,
+  vcColorPicker,
+  vcInput,
+  vcLayer,
+  vcSelect,
+  vcMessage,
+  vcNotification
 ]
 
 const install = app => {
-    components.forEach(item => {
-        if (item.__file) {
-            app.component(item.name, item)
-        } else {
-            app.provide(item.name, item.function)
-        }
-    })
+  components.forEach(item => {
+    if (item.__file) {
+      app.component(item.name, item)
+    } else {
+      app.provide(item.name, item.function)  
+    }
+  })
 }
 
 export default {
-    install,
-    vcButton,
-    vcForm,
-    vcFormItem,
-    vcIcon,
-    vcNotification,
-    vcInput,
-    vcMessage
+  install,
+  vcButton,
+  vcFormItem,
+  vcForm,
+  vcButtonGroup,
+  vcIcon,
+  vcColorPicker,
+  vcInput,
+  vcLayer,
+  vcSelect,
+  vcMessage,
+  vcNotification
 }
