e01d5f2b3af29b72a69b390bfcf7fd9aace86be0
wenlong <530675800@qq.com>
2020-08-05 23:11:39 +0800

    细节调整

--- packages/index.js
+++ packages/index.js
@@ -1,27 +1,27 @@
-/* 以下代码自动生成于 Sat Aug 01 2020 14:18:42 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Wed Aug 05 2020 22:52:09 GMT+0800 (中国标准时间) */
 
 import vcButton from './components/button/index.vue'
-import vcFormItem from './components/formItem/index.vue'
+import vcButtonGroup from './components/buttonGroup/index.vue'
 import vcColorPicker from './components/colorPicker/index.vue'
 import vcForm from './components/form/index.vue'
-import vcButtonGroup from './components/buttonGroup/index.vue'
 import vcIcon from './components/icon/index.vue'
-import vcSelect from './components/select/index.vue'
-import vcNotification from './components/notification/index.js'
-import vcMessage from './components/message/index.js'
+import vcFormItem from './components/formItem/index.vue'
 import vcLayer from './components/layer/index.vue'
+import vcMessage from './components/message/index.js'
+import vcNotification from './components/notification/index.js'
+import vcSelect from './components/select/index.vue'
 
 const components = [
   vcButton,
-  vcFormItem,
+  vcButtonGroup,
   vcColorPicker,
   vcForm,
-  vcButtonGroup,
   vcIcon,
-  vcSelect,
-  vcNotification,
+  vcFormItem,
+  vcLayer,
   vcMessage,
-  vcLayer
+  vcNotification,
+  vcSelect
 ]
 
 const install = app => {
@@ -37,13 +37,13 @@ const install = app => {
 export default {
   install,
   vcButton,
-  vcFormItem,
+  vcButtonGroup,
   vcColorPicker,
   vcForm,
-  vcButtonGroup,
   vcIcon,
-  vcSelect,
-  vcNotification,
+  vcFormItem,
+  vcLayer,
   vcMessage,
-  vcLayer
+  vcNotification,
+  vcSelect
 }
