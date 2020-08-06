da6f1c7bdf8864749fec736a0a3922906dbedcdc
wenlong <530675800@qq.com>
2020-08-06 17:18:18 +0800

    更新

--- packages/index.js
+++ packages/index.js
@@ -1,24 +1,24 @@
-/* 以下代码自动生成于 Wed Aug 05 2020 22:52:09 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Thu Aug 06 2020 17:17:44 GMT+0800 (中国标准时间) */
 
 import vcButton from './components/button/index.vue'
-import vcButtonGroup from './components/buttonGroup/index.vue'
 import vcColorPicker from './components/colorPicker/index.vue'
-import vcForm from './components/form/index.vue'
-import vcIcon from './components/icon/index.vue'
+import vcButtonGroup from './components/buttonGroup/index.vue'
 import vcFormItem from './components/formItem/index.vue'
+import vcForm from './components/form/index.vue'
 import vcLayer from './components/layer/index.vue'
+import vcIcon from './components/icon/index.vue'
 import vcMessage from './components/message/index.js'
 import vcNotification from './components/notification/index.js'
 import vcSelect from './components/select/index.vue'
 
 const components = [
   vcButton,
-  vcButtonGroup,
   vcColorPicker,
-  vcForm,
-  vcIcon,
+  vcButtonGroup,
   vcFormItem,
+  vcForm,
   vcLayer,
+  vcIcon,
   vcMessage,
   vcNotification,
   vcSelect
@@ -37,12 +37,12 @@ const install = app => {
 export default {
   install,
   vcButton,
-  vcButtonGroup,
   vcColorPicker,
-  vcForm,
-  vcIcon,
+  vcButtonGroup,
   vcFormItem,
+  vcForm,
   vcLayer,
+  vcIcon,
   vcMessage,
   vcNotification,
   vcSelect
