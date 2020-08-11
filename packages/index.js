54105a5dd4005aa40b5f4714dc17b160461514b2
wenlong <530675800@qq.com>
2020-08-11 09:15:05 +0800

    alpha.10.1

--- packages/index.js
+++ packages/index.js
@@ -1,30 +1,30 @@
-/* 以下代码自动生成于 Fri Aug 07 2020 14:15:14 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Tue Aug 11 2020 09:11:05 GMT+0800 (中国标准时间) */
 
 import vcButton from './components/button/index.vue'
 import vcButtonGroup from './components/buttonGroup/index.vue'
-import vcColorPicker from './components/colorPicker/index.vue'
-import vcFormItem from './components/formItem/index.vue'
 import vcForm from './components/form/index.vue'
+import vcFormItem from './components/formItem/index.vue'
+import vcColorPicker from './components/colorPicker/index.vue'
 import vcIcon from './components/icon/index.vue'
 import vcLayer from './components/layer/index.vue'
 import vcMessage from './components/message/index.js'
 import vcNotification from './components/notification/index.js'
-import vcSelect from './components/select/index.vue'
 import vcTabPane from './components/tabPane/index.vue'
+import vcSelect from './components/select/index.vue'
 import vcTabs from './components/tabs/index.vue'
 
 const components = [
   vcButton,
   vcButtonGroup,
-  vcColorPicker,
-  vcFormItem,
   vcForm,
+  vcFormItem,
+  vcColorPicker,
   vcIcon,
   vcLayer,
   vcMessage,
   vcNotification,
-  vcSelect,
   vcTabPane,
+  vcSelect,
   vcTabs
 ]
 
@@ -42,14 +42,14 @@ export default {
   install,
   vcButton,
   vcButtonGroup,
-  vcColorPicker,
-  vcFormItem,
   vcForm,
+  vcFormItem,
+  vcColorPicker,
   vcIcon,
   vcLayer,
   vcMessage,
   vcNotification,
-  vcSelect,
   vcTabPane,
+  vcSelect,
   vcTabs
 }
