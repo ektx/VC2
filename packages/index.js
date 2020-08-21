abc54e50360bea6c95c570e49a9d71b9ec79b91d
wenlong <530675800@qq.com>
2020-08-21 10:43:41 +0800

    alpha.11.1
    细节功能调整

--- packages/index.js
+++ packages/index.js
@@ -1,35 +1,35 @@
-/* 以下代码自动生成于 Mon Aug 17 2020 14:13:42 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Fri Aug 21 2020 10:41:28 GMT+0800 (中国标准时间) */
 
-import vcButtonGroup from './components/buttonGroup/index.vue'
 import vcButton from './components/button/index.vue'
 import vcForm from './components/form/index.vue'
 import vcColorPicker from './components/colorPicker/index.vue'
 import vcFormItem from './components/formItem/index.vue'
+import vcButtonGroup from './components/buttonGroup/index.vue'
+import vcLayer from './components/layer/index.vue'
 import vcIcon from './components/icon/index.vue'
 import vcInput from './components/input/index.vue'
-import vcLayer from './components/layer/index.vue'
-import vcNotification from './components/notification/index.js'
-import vcMessage from './components/message/index.js'
 import vcRadio from './components/radio/index.vue'
 import vcSelect from './components/select/index.vue'
 import vcSwitch from './components/switch/index.vue'
+import vcMessage from './components/message/index.js'
+import vcNotification from './components/notification/index.js'
 import vcTabPane from './components/tabPane/index.vue'
 import vcTabs from './components/tabs/index.vue'
 
 const components = [
-  vcButtonGroup,
   vcButton,
   vcForm,
   vcColorPicker,
   vcFormItem,
+  vcButtonGroup,
+  vcLayer,
   vcIcon,
   vcInput,
-  vcLayer,
-  vcNotification,
-  vcMessage,
   vcRadio,
   vcSelect,
   vcSwitch,
+  vcMessage,
+  vcNotification,
   vcTabPane,
   vcTabs
 ]
@@ -46,19 +46,19 @@ const install = app => {
 
 export default {
   install,
-  vcButtonGroup,
   vcButton,
   vcForm,
   vcColorPicker,
   vcFormItem,
+  vcButtonGroup,
+  vcLayer,
   vcIcon,
   vcInput,
-  vcLayer,
-  vcNotification,
-  vcMessage,
   vcRadio,
   vcSelect,
   vcSwitch,
+  vcMessage,
+  vcNotification,
   vcTabPane,
   vcTabs
 }
