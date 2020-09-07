9ecedca068ada37608697bb12dec31610c8c5492
wenlong <530675800@qq.com>
2020-09-07 16:02:39 +0800

    🚀alpha.13

--- packages/index.js
+++ packages/index.js
@@ -1,40 +1,40 @@
-/* 以下代码自动生成于 Mon Sep 07 2020 15:11:32 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Mon Sep 07 2020 16:00:20 GMT+0800 (中国标准时间) */
 
 import vcButton from './components/button/index.vue'
-import vcButtonGroup from './components/buttonGroup/index.vue'
 import vcCollapseItem from './components/collapseItem/index.vue'
 import vcCollapse from './components/collapse/index.vue'
 import vcColorPicker from './components/colorPicker/index.vue'
 import vcForm from './components/form/index.vue'
-import vcFormItem from './components/formItem/index.vue'
 import vcIcon from './components/icon/index.vue'
-import vcLayer from './components/layer/index.vue'
+import vcFormItem from './components/formItem/index.vue'
 import vcInput from './components/input/index.vue'
 import vcMessage from './components/message/index.js'
+import vcLayer from './components/layer/index.vue'
 import vcNotification from './components/notification/index.js'
 import vcRadio from './components/radio/index.vue'
 import vcSelect from './components/select/index.vue'
-import vcSwitch from './components/switch/index.vue'
 import vcTabPane from './components/tabPane/index.vue'
+import vcSwitch from './components/switch/index.vue'
+import vcButtonGroup from './components/buttonGroup/index.vue'
 import vcTabs from './components/tabs/index.vue'
 
 const components = [
   vcButton,
-  vcButtonGroup,
   vcCollapseItem,
   vcCollapse,
   vcColorPicker,
   vcForm,
-  vcFormItem,
   vcIcon,
-  vcLayer,
+  vcFormItem,
   vcInput,
   vcMessage,
+  vcLayer,
   vcNotification,
   vcRadio,
   vcSelect,
-  vcSwitch,
   vcTabPane,
+  vcSwitch,
+  vcButtonGroup,
   vcTabs
 ]
 
@@ -53,20 +53,20 @@ const install = app => {
 export default {
   install,
   vcButton,
-  vcButtonGroup,
   vcCollapseItem,
   vcCollapse,
   vcColorPicker,
   vcForm,
-  vcFormItem,
   vcIcon,
-  vcLayer,
+  vcFormItem,
   vcInput,
   vcMessage,
+  vcLayer,
   vcNotification,
   vcRadio,
   vcSelect,
-  vcSwitch,
   vcTabPane,
+  vcSwitch,
+  vcButtonGroup,
   vcTabs
 }
