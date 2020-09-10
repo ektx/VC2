3b7ed3e60b818a9af07b58adb23b0543855e8d8b
wenlong <530675800@qq.com>
2020-09-10 14:54:09 +0800

    0.0.0-alpha.14

--- packages/index.js
+++ packages/index.js
@@ -1,42 +1,42 @@
-/* 以下代码自动生成于 Thu Sep 10 2020 14:13:43 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Thu Sep 10 2020 14:54:05 GMT+0800 (中国标准时间) */
 
-import vcButton from './components/button/index.vue'
 import vcButtonGroup from './components/buttonGroup/index.vue'
+import vcButton from './components/button/index.vue'
 import vcCollapseItem from './components/collapseItem/index.vue'
 import vcCollapse from './components/collapse/index.vue'
-import vcForm from './components/form/index.vue'
-import vcFormItem from './components/formItem/index.vue'
 import vcColorPicker from './components/colorPicker/index.vue'
-import vcIcon from './components/icon/index.vue'
+import vcForm from './components/form/index.vue'
 import vcInput from './components/input/index.vue'
-import vcMessage from './components/message/index.js'
+import vcIcon from './components/icon/index.vue'
+import vcFormItem from './components/formItem/index.vue'
 import vcLayer from './components/layer/index.vue'
 import vcPagination from './components/pagination/index.vue'
-import vcNotification from './components/notification/index.js'
 import vcRadio from './components/radio/index.vue'
-import vcSelect from './components/select/index.vue'
+import vcMessage from './components/message/index.js'
+import vcNotification from './components/notification/index.js'
 import vcSwitch from './components/switch/index.vue'
+import vcSelect from './components/select/index.vue'
 import vcTabPane from './components/tabPane/index.vue'
 import vcTable from './components/table/index.vue'
 import vcTabs from './components/tabs/index.vue'
 
 const components = [
-  vcButton,
   vcButtonGroup,
+  vcButton,
   vcCollapseItem,
   vcCollapse,
-  vcForm,
-  vcFormItem,
   vcColorPicker,
-  vcIcon,
+  vcForm,
   vcInput,
-  vcMessage,
+  vcIcon,
+  vcFormItem,
   vcLayer,
   vcPagination,
-  vcNotification,
   vcRadio,
-  vcSelect,
+  vcMessage,
+  vcNotification,
   vcSwitch,
+  vcSelect,
   vcTabPane,
   vcTable,
   vcTabs
@@ -56,22 +56,22 @@ const install = app => {
 
 export default {
   install,
-  vcButton,
   vcButtonGroup,
+  vcButton,
   vcCollapseItem,
   vcCollapse,
-  vcForm,
-  vcFormItem,
   vcColorPicker,
-  vcIcon,
+  vcForm,
   vcInput,
-  vcMessage,
+  vcIcon,
+  vcFormItem,
   vcLayer,
   vcPagination,
-  vcNotification,
   vcRadio,
-  vcSelect,
+  vcMessage,
+  vcNotification,
   vcSwitch,
+  vcSelect,
   vcTabPane,
   vcTable,
   vcTabs
