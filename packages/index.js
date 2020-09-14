615a56260f723cd3158153fea6bc1185a1b370ab
wenlong <530675800@qq.com>
2020-09-14 13:54:14 +0800

    发布细节调整

--- packages/index.js
+++ packages/index.js
@@ -1,19 +1,19 @@
-/* 以下代码自动生成于 Mon Sep 14 2020 13:16:49 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Mon Sep 14 2020 13:47:16 GMT+0800 (中国标准时间) */
 
 import vcButton from './components/button/index.vue'
+import vcCollapse from './components/collapse/index.vue'
 import vcButtonGroup from './components/buttonGroup/index.vue'
 import vcCollapseItem from './components/collapseItem/index.vue'
 import vcColorPicker from './components/colorPicker/index.vue'
-import vcCollapse from './components/collapse/index.vue'
 import vcForm from './components/form/index.vue'
 import vcFormItem from './components/formItem/index.vue'
 import vcIcon from './components/icon/index.vue'
 import vcLayer from './components/layer/index.vue'
 import vcInput from './components/input/index.vue'
-import vcNotification from './components/notification/index.js'
 import vcMessage from './components/message/index.js'
-import vcRadio from './components/radio/index.vue'
+import vcNotification from './components/notification/index.js'
 import vcPagination from './components/pagination/index.vue'
+import vcRadio from './components/radio/index.vue'
 import vcSelect from './components/select/index.vue'
 import vcSwitch from './components/switch/index.vue'
 import vcTabPane from './components/tabPane/index.vue'
@@ -22,19 +22,19 @@ import vcTabs from './components/tabs/index.vue'
 
 const components = [
   vcButton,
+  vcCollapse,
   vcButtonGroup,
   vcCollapseItem,
   vcColorPicker,
-  vcCollapse,
   vcForm,
   vcFormItem,
   vcIcon,
   vcLayer,
   vcInput,
-  vcNotification,
   vcMessage,
-  vcRadio,
+  vcNotification,
   vcPagination,
+  vcRadio,
   vcSelect,
   vcSwitch,
   vcTabPane,
@@ -57,19 +57,19 @@ const install = app => {
 export default {
   install,
   vcButton,
+  vcCollapse,
   vcButtonGroup,
   vcCollapseItem,
   vcColorPicker,
-  vcCollapse,
   vcForm,
   vcFormItem,
   vcIcon,
   vcLayer,
   vcInput,
-  vcNotification,
   vcMessage,
-  vcRadio,
+  vcNotification,
   vcPagination,
+  vcRadio,
   vcSelect,
   vcSwitch,
   vcTabPane,
