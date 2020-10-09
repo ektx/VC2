fc3e2728385dce40878f0156b0c43dcb15ab32c1
wenlong <530675800@qq.com>
2020-10-09 16:45:00 +0800

    1.2.5

--- packages/index.js
+++ packages/index.js
@@ -1,12 +1,12 @@
-/* 以下代码自动生成于 Fri Oct 09 2020 16:31:06 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Fri Oct 09 2020 16:44:57 GMT+0800 (中国标准时间) */
 
 import vcButton from './components/button/index.vue'
 import vcButtonGroup from './components/buttonGroup/index.vue'
-import vcCollapseItem from './components/collapseItem/index.vue'
 import vcCollapse from './components/collapse/index.vue'
 import vcColorPicker from './components/colorPicker/index.vue'
-import vcForm from './components/form/index.vue'
+import vcCollapseItem from './components/collapseItem/index.vue'
 import vcConfirm from './components/confirm/index.js'
+import vcForm from './components/form/index.vue'
 import vcFormItem from './components/formItem/index.vue'
 import vcIcon from './components/icon/index.vue'
 import vcInput from './components/input/index.vue'
@@ -25,11 +25,11 @@ import vcTabs from './components/tabs/index.vue'
 const components = [
   vcButton,
   vcButtonGroup,
-  vcCollapseItem,
   vcCollapse,
   vcColorPicker,
-  vcForm,
+  vcCollapseItem,
   vcConfirm,
+  vcForm,
   vcFormItem,
   vcIcon,
   vcInput,
@@ -59,15 +59,15 @@ const install = app => {
 }
 
 export default {
-  version: "1.2.4",
+  version: "1.2.5",
   install,
   vcButton,
   vcButtonGroup,
-  vcCollapseItem,
   vcCollapse,
   vcColorPicker,
-  vcForm,
+  vcCollapseItem,
   vcConfirm,
+  vcForm,
   vcFormItem,
   vcIcon,
   vcInput,
