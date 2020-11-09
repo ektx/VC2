13605d9b501f6fc02650a4ba2b8c2ade830e8d27
wenlong <530675800@qq.com>
2020-11-09 17:02:39 +0800

    1.7.0

--- packages/index.js
+++ packages/index.js
@@ -1,8 +1,8 @@
-/* 以下代码自动生成于 Mon Nov 09 2020 10:29:09 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Mon Nov 09 2020 17:02:37 GMT+0800 (中国标准时间) */
 
 import vcButton from './components/button/index.vue'
-import vcCollapse from './components/collapse/index.vue'
 import vcButtonGroup from './components/buttonGroup/index.vue'
+import vcCollapse from './components/collapse/index.vue'
 import vcColorPicker from './components/colorPicker/index.vue'
 import vcCollapseItem from './components/collapseItem/index.vue'
 import vcConfirm from './components/confirm/index.js'
@@ -17,18 +17,18 @@ import vcNotification from './components/notification/index.js'
 import vcOperationList from './components/operationList/index.vue'
 import vcPagination from './components/pagination/index.vue'
 import vcPopover from './components/popover/index.vue'
-import vcRadioGroup from './components/radioGroup/index.vue'
 import vcRadio from './components/radio/index.vue'
-import vcSwitch from './components/switch/index.vue'
+import vcRadioGroup from './components/radioGroup/index.vue'
 import vcSelect from './components/select/index.vue'
+import vcSwitch from './components/switch/index.vue'
 import vcTabPane from './components/tabPane/index.vue'
 import vcTable from './components/table/index.vue'
 import vcTabs from './components/tabs/index.vue'
 
 const components = [
   vcButton,
-  vcCollapse,
   vcButtonGroup,
+  vcCollapse,
   vcColorPicker,
   vcCollapseItem,
   vcConfirm,
@@ -43,10 +43,10 @@ const components = [
   vcOperationList,
   vcPagination,
   vcPopover,
-  vcRadioGroup,
   vcRadio,
-  vcSwitch,
+  vcRadioGroup,
   vcSelect,
+  vcSwitch,
   vcTabPane,
   vcTable,
   vcTabs
@@ -65,11 +65,11 @@ const install = app => {
 }
 
 export default {
-  version: "1.6.0",
+  version: "1.7.0",
   install,
   vcButton,
-  vcCollapse,
   vcButtonGroup,
+  vcCollapse,
   vcColorPicker,
   vcCollapseItem,
   vcConfirm,
@@ -84,10 +84,10 @@ export default {
   vcOperationList,
   vcPagination,
   vcPopover,
-  vcRadioGroup,
   vcRadio,
-  vcSwitch,
+  vcRadioGroup,
   vcSelect,
+  vcSwitch,
   vcTabPane,
   vcTable,
   vcTabs
