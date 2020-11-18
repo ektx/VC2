ac3ecbe120032a83a9c2b75ac1d0047aff649a49
wenlong <530675800@qq.com>
2020-11-18 16:36:51 +0800

    1.7.3

--- packages/index.js
+++ packages/index.js
@@ -1,26 +1,26 @@
-/* 以下代码自动生成于 Wed Nov 18 2020 10:17:36 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Wed Nov 18 2020 16:36:48 GMT+0800 (中国标准时间) */
 
 import vcButton from './components/button/index.vue'
 import vcButtonGroup from './components/buttonGroup/index.vue'
-import vcCollapse from './components/collapse/index.vue'
 import vcCollapseItem from './components/collapseItem/index.vue'
-import vcColorPicker from './components/colorPicker/index.vue'
+import vcCollapse from './components/collapse/index.vue'
 import vcConfirm from './components/confirm/index.js'
+import vcColorPicker from './components/colorPicker/index.vue'
 import vcForm from './components/form/index.vue'
 import vcFormItem from './components/formItem/index.vue'
 import vcIcon from './components/icon/index.vue'
+import vcInputNumber from './components/inputNumber/index.vue'
 import vcInput from './components/input/index.vue'
 import vcLayer from './components/layer/index.vue'
-import vcInputNumber from './components/inputNumber/index.vue'
 import vcMessage from './components/message/index.js'
-import vcOperationList from './components/operationList/index.vue'
 import vcNotification from './components/notification/index.js'
+import vcOperationList from './components/operationList/index.vue'
 import vcPagination from './components/pagination/index.vue'
 import vcPopover from './components/popover/index.vue'
 import vcRadio from './components/radio/index.vue'
 import vcRadioGroup from './components/radioGroup/index.vue'
-import vcSwitch from './components/switch/index.vue'
 import vcSelect from './components/select/index.vue'
+import vcSwitch from './components/switch/index.vue'
 import vcTabPane from './components/tabPane/index.vue'
 import vcTable from './components/table/index.vue'
 import vcTabs from './components/tabs/index.vue'
@@ -28,25 +28,25 @@ import vcTabs from './components/tabs/index.vue'
 const components = [
   vcButton,
   vcButtonGroup,
-  vcCollapse,
   vcCollapseItem,
-  vcColorPicker,
+  vcCollapse,
   vcConfirm,
+  vcColorPicker,
   vcForm,
   vcFormItem,
   vcIcon,
+  vcInputNumber,
   vcInput,
   vcLayer,
-  vcInputNumber,
   vcMessage,
-  vcOperationList,
   vcNotification,
+  vcOperationList,
   vcPagination,
   vcPopover,
   vcRadio,
   vcRadioGroup,
-  vcSwitch,
   vcSelect,
+  vcSwitch,
   vcTabPane,
   vcTable,
   vcTabs
@@ -65,29 +65,29 @@ const install = app => {
 }
 
 export default {
-  version: "1.7.2",
+  version: "1.7.3",
   install,
   vcButton,
   vcButtonGroup,
-  vcCollapse,
   vcCollapseItem,
-  vcColorPicker,
+  vcCollapse,
   vcConfirm,
+  vcColorPicker,
   vcForm,
   vcFormItem,
   vcIcon,
+  vcInputNumber,
   vcInput,
   vcLayer,
-  vcInputNumber,
   vcMessage,
-  vcOperationList,
   vcNotification,
+  vcOperationList,
   vcPagination,
   vcPopover,
   vcRadio,
   vcRadioGroup,
-  vcSwitch,
   vcSelect,
+  vcSwitch,
   vcTabPane,
   vcTable,
   vcTabs
