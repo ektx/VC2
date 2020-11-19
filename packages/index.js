8d0b6651a9f0590b85a489c1023d446f6e77ad2f
wenlong <530675800@qq.com>
2020-11-19 14:29:26 +0800

    styles:更新样式引用

--- packages/index.js
+++ packages/index.js
@@ -1,29 +1,29 @@
-/* 以下代码自动生成于 Thu Nov 19 2020 14:25:28 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Thu Nov 19 2020 14:28:56 GMT+0800 (中国标准时间) */
 
 import vcButton from './components/button/index.vue'
 import vcButtonGroup from './components/buttonGroup/index.vue'
 import vcCollapseItem from './components/collapseItem/index.vue'
 import vcCollapse from './components/collapse/index.vue'
-import vcColorPicker from './components/colorPicker/index.vue'
 import vcConfirm from './components/confirm/index.js'
 import vcForm from './components/form/index.vue'
+import vcColorPicker from './components/colorPicker/index.vue'
 import vcFormItem from './components/formItem/index.vue'
 import vcIcon from './components/icon/index.vue'
+import vcInputNumber from './components/inputNumber/index.vue'
 import vcInput from './components/input/index.vue'
 import vcLayer from './components/layer/index.vue'
-import vcInputNumber from './components/inputNumber/index.vue'
-import vcNotification from './components/notification/index.js'
 import vcMessage from './components/message/index.js'
+import vcNotification from './components/notification/index.js'
 import vcOperationList from './components/operationList/index.vue'
 import vcPagination from './components/pagination/index.vue'
 import vcPopover from './components/popover/index.vue'
 import vcRadio from './components/radio/index.vue'
 import vcRadioGroup from './components/radioGroup/index.vue'
+import vcSelect from './components/select/index.vue'
 import vcSwitch from './components/switch/index.vue'
 import vcTabPane from './components/tabPane/index.vue'
 import vcTable from './components/table/index.vue'
 import vcTabs from './components/tabs/index.vue'
-import vcSelect from './components/select/index.vue'
 import vcTimePicker from './components/timePicker/index.vue'
 
 const components = [
@@ -31,26 +31,26 @@ const components = [
   vcButtonGroup,
   vcCollapseItem,
   vcCollapse,
-  vcColorPicker,
   vcConfirm,
   vcForm,
+  vcColorPicker,
   vcFormItem,
   vcIcon,
+  vcInputNumber,
   vcInput,
   vcLayer,
-  vcInputNumber,
-  vcNotification,
   vcMessage,
+  vcNotification,
   vcOperationList,
   vcPagination,
   vcPopover,
   vcRadio,
   vcRadioGroup,
+  vcSelect,
   vcSwitch,
   vcTabPane,
   vcTable,
   vcTabs,
-  vcSelect,
   vcTimePicker
 ]
 
@@ -73,25 +73,25 @@ export default {
   vcButtonGroup,
   vcCollapseItem,
   vcCollapse,
-  vcColorPicker,
   vcConfirm,
   vcForm,
+  vcColorPicker,
   vcFormItem,
   vcIcon,
+  vcInputNumber,
   vcInput,
   vcLayer,
-  vcInputNumber,
-  vcNotification,
   vcMessage,
+  vcNotification,
   vcOperationList,
   vcPagination,
   vcPopover,
   vcRadio,
   vcRadioGroup,
+  vcSelect,
   vcSwitch,
   vcTabPane,
   vcTable,
   vcTabs,
-  vcSelect,
   vcTimePicker
 }
