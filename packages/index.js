112af94345eafe7f271dd605bdf6573b6dfefc72
wenlong <530675800@qq.com>
2020-11-19 14:25:31 +0800

    1.8.0

--- packages/index.js
+++ packages/index.js
@@ -1,56 +1,56 @@
-/* 以下代码自动生成于 Thu Nov 19 2020 14:22:19 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Thu Nov 19 2020 14:25:28 GMT+0800 (中国标准时间) */
 
 import vcButton from './components/button/index.vue'
 import vcButtonGroup from './components/buttonGroup/index.vue'
+import vcCollapseItem from './components/collapseItem/index.vue'
 import vcCollapse from './components/collapse/index.vue'
 import vcColorPicker from './components/colorPicker/index.vue'
-import vcCollapseItem from './components/collapseItem/index.vue'
 import vcConfirm from './components/confirm/index.js'
 import vcForm from './components/form/index.vue'
 import vcFormItem from './components/formItem/index.vue'
 import vcIcon from './components/icon/index.vue'
 import vcInput from './components/input/index.vue'
-import vcInputNumber from './components/inputNumber/index.vue'
 import vcLayer from './components/layer/index.vue'
+import vcInputNumber from './components/inputNumber/index.vue'
+import vcNotification from './components/notification/index.js'
 import vcMessage from './components/message/index.js'
 import vcOperationList from './components/operationList/index.vue'
-import vcNotification from './components/notification/index.js'
-import vcPopover from './components/popover/index.vue'
 import vcPagination from './components/pagination/index.vue'
+import vcPopover from './components/popover/index.vue'
 import vcRadio from './components/radio/index.vue'
-import vcSwitch from './components/switch/index.vue'
-import vcSelect from './components/select/index.vue'
 import vcRadioGroup from './components/radioGroup/index.vue'
+import vcSwitch from './components/switch/index.vue'
 import vcTabPane from './components/tabPane/index.vue'
 import vcTable from './components/table/index.vue'
 import vcTabs from './components/tabs/index.vue'
+import vcSelect from './components/select/index.vue'
 import vcTimePicker from './components/timePicker/index.vue'
 
 const components = [
   vcButton,
   vcButtonGroup,
+  vcCollapseItem,
   vcCollapse,
   vcColorPicker,
-  vcCollapseItem,
   vcConfirm,
   vcForm,
   vcFormItem,
   vcIcon,
   vcInput,
-  vcInputNumber,
   vcLayer,
+  vcInputNumber,
+  vcNotification,
   vcMessage,
   vcOperationList,
-  vcNotification,
-  vcPopover,
   vcPagination,
+  vcPopover,
   vcRadio,
-  vcSwitch,
-  vcSelect,
   vcRadioGroup,
+  vcSwitch,
   vcTabPane,
   vcTable,
   vcTabs,
+  vcSelect,
   vcTimePicker
 ]
 
@@ -67,31 +67,31 @@ const install = app => {
 }
 
 export default {
-  version: "1.7.3",
+  version: "1.8.0",
   install,
   vcButton,
   vcButtonGroup,
+  vcCollapseItem,
   vcCollapse,
   vcColorPicker,
-  vcCollapseItem,
   vcConfirm,
   vcForm,
   vcFormItem,
   vcIcon,
   vcInput,
-  vcInputNumber,
   vcLayer,
+  vcInputNumber,
+  vcNotification,
   vcMessage,
   vcOperationList,
-  vcNotification,
-  vcPopover,
   vcPagination,
+  vcPopover,
   vcRadio,
-  vcSwitch,
-  vcSelect,
   vcRadioGroup,
+  vcSwitch,
   vcTabPane,
   vcTable,
   vcTabs,
+  vcSelect,
   vcTimePicker
 }
