fe09260f91969f3f7af129642d79fccf02eee438
wenlong <530675800@qq.com>
2020-11-23 13:57:15 +0800

    feat:添加按需加载功能
    更好的支持 composition api功能

--- packages/index.js
+++ packages/index.js
@@ -1,57 +1,57 @@
-/* 以下代码自动生成于 Fri Nov 20 2020 14:28:26 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Mon Nov 23 2020 13:55:12 GMT+0800 (中国标准时间) */
 
-import vcButton from './components/button/index.vue'
-import vcButtonGroup from './components/buttonGroup/index.vue'
-import vcCollapse from './components/collapse/index.vue'
-import vcColorPicker from './components/colorPicker/index.vue'
-import vcCollapseItem from './components/collapseItem/index.vue'
-import vcConfirm from './components/confirm/index.js'
-import vcForm from './components/form/index.vue'
-import vcFormItem from './components/formItem/index.vue'
-import vcIcon from './components/icon/index.vue'
-import vcInput from './components/input/index.vue'
-import vcInputNumber from './components/inputNumber/index.vue'
-import vcLayer from './components/layer/index.vue'
-import vcMessage from './components/message/index.js'
-import vcOperationList from './components/operationList/index.vue'
-import vcNotification from './components/notification/index.js'
-import vcPagination from './components/pagination/index.vue'
-import vcPopover from './components/popover/index.vue'
-import vcRadio from './components/radio/index.vue'
-import vcRadioGroup from './components/radioGroup/index.vue'
-import vcSelect from './components/select/index.vue'
-import vcSwitch from './components/switch/index.vue'
-import vcTabPane from './components/tabPane/index.vue'
-import vcTable from './components/table/index.vue'
-import vcTabs from './components/tabs/index.vue'
-import vcTimePicker from './components/timePicker/index.vue'
+import Button from './components/button/index.vue'
+import Collapse from './components/collapse/index.vue'
+import ButtonGroup from './components/buttonGroup/index.vue'
+import CollapseItem from './components/collapseItem/index.vue'
+import ColorPicker from './components/colorPicker/index.vue'
+import Confirm from './components/confirm/index.js'
+import Form from './components/form/index.vue'
+import FormItem from './components/formItem/index.vue'
+import Icon from './components/icon/index.vue'
+import Input from './components/input/index.vue'
+import InputNumber from './components/inputNumber/index.vue'
+import Layer from './components/layer/index.vue'
+import Message from './components/message/index.js'
+import Notification from './components/notification/index.js'
+import OperationList from './components/operationList/index.vue'
+import Pagination from './components/pagination/index.vue'
+import Popover from './components/popover/index.vue'
+import Radio from './components/radio/index.vue'
+import RadioGroup from './components/radioGroup/index.vue'
+import Select from './components/select/index.vue'
+import Switch from './components/switch/index.vue'
+import TabPane from './components/tabPane/index.vue'
+import Table from './components/table/index.vue'
+import Tabs from './components/tabs/index.vue'
+import TimePicker from './components/timePicker/index.vue'
 
 const components = [
-  vcButton,
-  vcButtonGroup,
-  vcCollapse,
-  vcColorPicker,
-  vcCollapseItem,
-  vcConfirm,
-  vcForm,
-  vcFormItem,
-  vcIcon,
-  vcInput,
-  vcInputNumber,
-  vcLayer,
-  vcMessage,
-  vcOperationList,
-  vcNotification,
-  vcPagination,
-  vcPopover,
-  vcRadio,
-  vcRadioGroup,
-  vcSelect,
-  vcSwitch,
-  vcTabPane,
-  vcTable,
-  vcTabs,
-  vcTimePicker
+  Button,
+  Collapse,
+  ButtonGroup,
+  CollapseItem,
+  ColorPicker,
+  Confirm,
+  Form,
+  FormItem,
+  Icon,
+  Input,
+  InputNumber,
+  Layer,
+  Message,
+  Notification,
+  OperationList,
+  Pagination,
+  Popover,
+  Radio,
+  RadioGroup,
+  Select,
+  Switch,
+  TabPane,
+  Table,
+  Tabs,
+  TimePicker
 ]
 
 const install = app => {
@@ -60,38 +60,65 @@ const install = app => {
       app.component(item.name, item)
     } else {
       app.provide(item.name, item.function)  
-      // 绑定 ctx 上
+      // 绑定 ctx 上(不建议使用全局，后期将要移除)
+      // 建议动态导入
       app.config.globalProperties[item.name] = item.function
     }
   })
 }
 
+export const vcButton = Button
+export const vcCollapse = Collapse
+export const vcButtonGroup = ButtonGroup
+export const vcCollapseItem = CollapseItem
+export const vcColorPicker = ColorPicker
+export const vcConfirm = Confirm.function
+export const vcForm = Form
+export const vcFormItem = FormItem
+export const vcIcon = Icon
+export const vcInput = Input
+export const vcInputNumber = InputNumber
+export const vcLayer = Layer
+export const vcMessage = Message.function
+export const vcNotification = Notification.function
+export const vcOperationList = OperationList
+export const vcPagination = Pagination
+export const vcPopover = Popover
+export const vcRadio = Radio
+export const vcRadioGroup = RadioGroup
+export const vcSelect = Select
+export const vcSwitch = Switch
+export const vcTabPane = TabPane
+export const vcTable = Table
+export const vcTabs = Tabs
+export const vcTimePicker = TimePicker
+
 export default {
   version: "1.8.1",
   install,
-  vcButton,
-  vcButtonGroup,
-  vcCollapse,
-  vcColorPicker,
-  vcCollapseItem,
-  vcConfirm,
-  vcForm,
-  vcFormItem,
-  vcIcon,
-  vcInput,
-  vcInputNumber,
-  vcLayer,
-  vcMessage,
-  vcOperationList,
-  vcNotification,
-  vcPagination,
-  vcPopover,
-  vcRadio,
-  vcRadioGroup,
-  vcSelect,
-  vcSwitch,
-  vcTabPane,
-  vcTable,
-  vcTabs,
-  vcTimePicker
+  Button,
+  Collapse,
+  ButtonGroup,
+  CollapseItem,
+  ColorPicker,
+  Confirm,
+  Form,
+  FormItem,
+  Icon,
+  Input,
+  InputNumber,
+  Layer,
+  Message,
+  Notification,
+  OperationList,
+  Pagination,
+  Popover,
+  Radio,
+  RadioGroup,
+  Select,
+  Switch,
+  TabPane,
+  Table,
+  Tabs,
+  TimePicker
 }
