4e17deb985a510e2a12989f89758ad805ce57c41
wenlong <530675800@qq.com>
2020-11-24 13:44:42 +0800

    1.8.3

--- packages/index.js
+++ packages/index.js
@@ -1,7 +1,7 @@
-/* 以下代码自动生成于 Mon Nov 23 2020 14:54:47 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Tue Nov 24 2020 13:44:38 GMT+0800 (中国标准时间) */
 
-import ButtonGroup from './components/buttonGroup/index.vue'
 import Button from './components/button/index.vue'
+import ButtonGroup from './components/buttonGroup/index.vue'
 import Collapse from './components/collapse/index.vue'
 import CollapseItem from './components/collapseItem/index.vue'
 import ColorPicker from './components/colorPicker/index.vue'
@@ -9,12 +9,12 @@ import Confirm from './components/confirm/index.js'
 import Form from './components/form/index.vue'
 import FormItem from './components/formItem/index.vue'
 import Icon from './components/icon/index.vue'
-import Input from './components/input/index.vue'
 import InputNumber from './components/inputNumber/index.vue'
+import Input from './components/input/index.vue'
 import Layer from './components/layer/index.vue'
 import Message from './components/message/index.js'
-import OperationList from './components/operationList/index.vue'
 import Notification from './components/notification/index.js'
+import OperationList from './components/operationList/index.vue'
 import Pagination from './components/pagination/index.vue'
 import Popover from './components/popover/index.vue'
 import Radio from './components/radio/index.vue'
@@ -27,8 +27,8 @@ import Tabs from './components/tabs/index.vue'
 import TimePicker from './components/timePicker/index.vue'
 
 const components = [
-  ButtonGroup,
   Button,
+  ButtonGroup,
   Collapse,
   CollapseItem,
   ColorPicker,
@@ -36,12 +36,12 @@ const components = [
   Form,
   FormItem,
   Icon,
-  Input,
   InputNumber,
+  Input,
   Layer,
   Message,
-  OperationList,
   Notification,
+  OperationList,
   Pagination,
   Popover,
   Radio,
@@ -67,8 +67,8 @@ const install = app => {
   })
 }
 
-export const vcButtonGroup = ButtonGroup
 export const vcButton = Button
+export const vcButtonGroup = ButtonGroup
 export const vcCollapse = Collapse
 export const vcCollapseItem = CollapseItem
 export const vcColorPicker = ColorPicker
@@ -76,12 +76,12 @@ export const vcConfirm = Confirm.function
 export const vcForm = Form
 export const vcFormItem = FormItem
 export const vcIcon = Icon
-export const vcInput = Input
 export const vcInputNumber = InputNumber
+export const vcInput = Input
 export const vcLayer = Layer
 export const vcMessage = Message.function
-export const vcOperationList = OperationList
 export const vcNotification = Notification.function
+export const vcOperationList = OperationList
 export const vcPagination = Pagination
 export const vcPopover = Popover
 export const vcRadio = Radio
@@ -94,10 +94,10 @@ export const vcTabs = Tabs
 export const vcTimePicker = TimePicker
 
 export default {
-  version: "1.8.2",
+  version: "1.8.3",
   install,
-  ButtonGroup,
   Button,
+  ButtonGroup,
   Collapse,
   CollapseItem,
   ColorPicker,
@@ -105,12 +105,12 @@ export default {
   Form,
   FormItem,
   Icon,
-  Input,
   InputNumber,
+  Input,
   Layer,
   Message,
-  OperationList,
   Notification,
+  OperationList,
   Pagination,
   Popover,
   Radio,
