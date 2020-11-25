daf087a098587585b8b1cff903b2324d0c7e25f3
wenlong <530675800@qq.com>
2020-11-25 14:50:53 +0800

    styles:样式显示优化

--- packages/index.js
+++ packages/index.js
@@ -1,27 +1,27 @@
-/* 以下代码自动生成于 Tue Nov 24 2020 13:44:38 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Wed Nov 25 2020 14:31:29 GMT+0800 (中国标准时间) */
 
 import Button from './components/button/index.vue'
 import ButtonGroup from './components/buttonGroup/index.vue'
 import Collapse from './components/collapse/index.vue'
 import CollapseItem from './components/collapseItem/index.vue'
 import ColorPicker from './components/colorPicker/index.vue'
-import Confirm from './components/confirm/index.js'
 import Form from './components/form/index.vue'
+import Confirm from './components/confirm/index.js'
 import FormItem from './components/formItem/index.vue'
 import Icon from './components/icon/index.vue'
-import InputNumber from './components/inputNumber/index.vue'
 import Input from './components/input/index.vue'
 import Layer from './components/layer/index.vue'
+import InputNumber from './components/inputNumber/index.vue'
 import Message from './components/message/index.js'
 import Notification from './components/notification/index.js'
 import OperationList from './components/operationList/index.vue'
 import Pagination from './components/pagination/index.vue'
 import Popover from './components/popover/index.vue'
 import Radio from './components/radio/index.vue'
-import RadioGroup from './components/radioGroup/index.vue'
 import Select from './components/select/index.vue'
-import Switch from './components/switch/index.vue'
+import RadioGroup from './components/radioGroup/index.vue'
 import TabPane from './components/tabPane/index.vue'
+import Switch from './components/switch/index.vue'
 import Table from './components/table/index.vue'
 import Tabs from './components/tabs/index.vue'
 import TimePicker from './components/timePicker/index.vue'
@@ -32,23 +32,23 @@ const components = [
   Collapse,
   CollapseItem,
   ColorPicker,
-  Confirm,
   Form,
+  Confirm,
   FormItem,
   Icon,
-  InputNumber,
   Input,
   Layer,
+  InputNumber,
   Message,
   Notification,
   OperationList,
   Pagination,
   Popover,
   Radio,
-  RadioGroup,
   Select,
-  Switch,
+  RadioGroup,
   TabPane,
+  Switch,
   Table,
   Tabs,
   TimePicker
@@ -72,23 +72,23 @@ export const vcButtonGroup = ButtonGroup
 export const vcCollapse = Collapse
 export const vcCollapseItem = CollapseItem
 export const vcColorPicker = ColorPicker
-export const vcConfirm = Confirm.function
 export const vcForm = Form
+export const vcConfirm = Confirm.function
 export const vcFormItem = FormItem
 export const vcIcon = Icon
-export const vcInputNumber = InputNumber
 export const vcInput = Input
 export const vcLayer = Layer
+export const vcInputNumber = InputNumber
 export const vcMessage = Message.function
 export const vcNotification = Notification.function
 export const vcOperationList = OperationList
 export const vcPagination = Pagination
 export const vcPopover = Popover
 export const vcRadio = Radio
-export const vcRadioGroup = RadioGroup
 export const vcSelect = Select
-export const vcSwitch = Switch
+export const vcRadioGroup = RadioGroup
 export const vcTabPane = TabPane
+export const vcSwitch = Switch
 export const vcTable = Table
 export const vcTabs = Tabs
 export const vcTimePicker = TimePicker
@@ -101,23 +101,23 @@ export default {
   Collapse,
   CollapseItem,
   ColorPicker,
-  Confirm,
   Form,
+  Confirm,
   FormItem,
   Icon,
-  InputNumber,
   Input,
   Layer,
+  InputNumber,
   Message,
   Notification,
   OperationList,
   Pagination,
   Popover,
   Radio,
-  RadioGroup,
   Select,
-  Switch,
+  RadioGroup,
   TabPane,
+  Switch,
   Table,
   Tabs,
   TimePicker
