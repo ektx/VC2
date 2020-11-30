d8d8625a4f723dbd9398ad91fc23dd10f5a16032
ektx <530675800@qq.com>
2020-11-30 17:15:51 +0800

    styles:优化弹层效果

--- packages/index.js
+++ packages/index.js
@@ -1,57 +1,57 @@
-/* 以下代码自动生成于 Mon Nov 30 2020 16:31:46 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Mon Nov 30 2020 17:14:59 GMT+0800 (中国标准时间) */
 
 import Button from './components/button/index.vue'
-import ButtonGroup from './components/buttonGroup/index.vue'
 import Collapse from './components/collapse/index.vue'
-import ColorPicker from './components/colorPicker/index.vue'
-import Confirm from './components/confirm/index.js'
 import CollapseItem from './components/collapseItem/index.vue'
+import ButtonGroup from './components/buttonGroup/index.vue'
+import Confirm from './components/confirm/index.js'
+import ColorPicker from './components/colorPicker/index.vue'
 import Form from './components/form/index.vue'
 import FormItem from './components/formItem/index.vue'
 import Icon from './components/icon/index.vue'
 import Input from './components/input/index.vue'
-import Layer from './components/layer/index.vue'
 import InputNumber from './components/inputNumber/index.vue'
-import Notification from './components/notification/index.js'
+import Layer from './components/layer/index.vue'
 import Message from './components/message/index.js'
+import Notification from './components/notification/index.js'
 import OperationList from './components/operationList/index.vue'
 import Pagination from './components/pagination/index.vue'
-import Progress from './components/progress/index.vue'
 import Popover from './components/popover/index.vue'
-import RadioGroup from './components/radioGroup/index.vue'
+import Progress from './components/progress/index.vue'
 import Radio from './components/radio/index.vue'
-import Select from './components/select/index.vue'
+import RadioGroup from './components/radioGroup/index.vue'
 import Switch from './components/switch/index.vue'
-import Table from './components/table/index.vue'
 import TabPane from './components/tabPane/index.vue'
+import Table from './components/table/index.vue'
+import Select from './components/select/index.vue'
 import Tabs from './components/tabs/index.vue'
 import TimePicker from './components/timePicker/index.vue'
 
 const components = [
   Button,
-  ButtonGroup,
   Collapse,
-  ColorPicker,
-  Confirm,
   CollapseItem,
+  ButtonGroup,
+  Confirm,
+  ColorPicker,
   Form,
   FormItem,
   Icon,
   Input,
-  Layer,
   InputNumber,
-  Notification,
+  Layer,
   Message,
+  Notification,
   OperationList,
   Pagination,
-  Progress,
   Popover,
-  RadioGroup,
+  Progress,
   Radio,
-  Select,
+  RadioGroup,
   Switch,
-  Table,
   TabPane,
+  Table,
+  Select,
   Tabs,
   TimePicker
 ]
@@ -70,29 +70,29 @@ const install = app => {
 }
 
 export const vcButton = Button
-export const vcButtonGroup = ButtonGroup
 export const vcCollapse = Collapse
-export const vcColorPicker = ColorPicker
-export const vcConfirm = Confirm.function
 export const vcCollapseItem = CollapseItem
+export const vcButtonGroup = ButtonGroup
+export const vcConfirm = Confirm.function
+export const vcColorPicker = ColorPicker
 export const vcForm = Form
 export const vcFormItem = FormItem
 export const vcIcon = Icon
 export const vcInput = Input
-export const vcLayer = Layer
 export const vcInputNumber = InputNumber
-export const vcNotification = Notification.function
+export const vcLayer = Layer
 export const vcMessage = Message.function
+export const vcNotification = Notification.function
 export const vcOperationList = OperationList
 export const vcPagination = Pagination
-export const vcProgress = Progress
 export const vcPopover = Popover
-export const vcRadioGroup = RadioGroup
+export const vcProgress = Progress
 export const vcRadio = Radio
-export const vcSelect = Select
+export const vcRadioGroup = RadioGroup
 export const vcSwitch = Switch
-export const vcTable = Table
 export const vcTabPane = TabPane
+export const vcTable = Table
+export const vcSelect = Select
 export const vcTabs = Tabs
 export const vcTimePicker = TimePicker
 
@@ -100,29 +100,29 @@ export default {
   version: "1.9.0",
   install,
   Button,
-  ButtonGroup,
   Collapse,
-  ColorPicker,
-  Confirm,
   CollapseItem,
+  ButtonGroup,
+  Confirm,
+  ColorPicker,
   Form,
   FormItem,
   Icon,
   Input,
-  Layer,
   InputNumber,
-  Notification,
+  Layer,
   Message,
+  Notification,
   OperationList,
   Pagination,
-  Progress,
   Popover,
-  RadioGroup,
+  Progress,
   Radio,
-  Select,
+  RadioGroup,
   Switch,
-  Table,
   TabPane,
+  Table,
+  Select,
   Tabs,
   TimePicker
 }
