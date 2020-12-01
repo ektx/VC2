c8f68b18df5723d3bc19e3723c05ae23d8a2f57d
ektx <530675800@qq.com>
2020-12-01 15:15:43 +0800

    perf:使用函数方式管理动画

--- packages/index.js
+++ packages/index.js
@@ -1,11 +1,11 @@
-/* 以下代码自动生成于 Mon Nov 30 2020 17:14:59 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Tue Dec 01 2020 15:11:29 GMT+0800 (中国标准时间) */
 
 import Button from './components/button/index.vue'
 import Collapse from './components/collapse/index.vue'
-import CollapseItem from './components/collapseItem/index.vue'
 import ButtonGroup from './components/buttonGroup/index.vue'
-import Confirm from './components/confirm/index.js'
+import CollapseItem from './components/collapseItem/index.vue'
 import ColorPicker from './components/colorPicker/index.vue'
+import Confirm from './components/confirm/index.js'
 import Form from './components/form/index.vue'
 import FormItem from './components/formItem/index.vue'
 import Icon from './components/icon/index.vue'
@@ -14,26 +14,26 @@ import InputNumber from './components/inputNumber/index.vue'
 import Layer from './components/layer/index.vue'
 import Message from './components/message/index.js'
 import Notification from './components/notification/index.js'
-import OperationList from './components/operationList/index.vue'
 import Pagination from './components/pagination/index.vue'
-import Popover from './components/popover/index.vue'
 import Progress from './components/progress/index.vue'
+import OperationList from './components/operationList/index.vue'
+import Popover from './components/popover/index.vue'
 import Radio from './components/radio/index.vue'
 import RadioGroup from './components/radioGroup/index.vue'
+import Select from './components/select/index.vue'
 import Switch from './components/switch/index.vue'
 import TabPane from './components/tabPane/index.vue'
 import Table from './components/table/index.vue'
-import Select from './components/select/index.vue'
 import Tabs from './components/tabs/index.vue'
 import TimePicker from './components/timePicker/index.vue'
 
 const components = [
   Button,
   Collapse,
-  CollapseItem,
   ButtonGroup,
-  Confirm,
+  CollapseItem,
   ColorPicker,
+  Confirm,
   Form,
   FormItem,
   Icon,
@@ -42,16 +42,16 @@ const components = [
   Layer,
   Message,
   Notification,
-  OperationList,
   Pagination,
-  Popover,
   Progress,
+  OperationList,
+  Popover,
   Radio,
   RadioGroup,
+  Select,
   Switch,
   TabPane,
   Table,
-  Select,
   Tabs,
   TimePicker
 ]
@@ -71,10 +71,10 @@ const install = app => {
 
 export const vcButton = Button
 export const vcCollapse = Collapse
-export const vcCollapseItem = CollapseItem
 export const vcButtonGroup = ButtonGroup
-export const vcConfirm = Confirm.function
+export const vcCollapseItem = CollapseItem
 export const vcColorPicker = ColorPicker
+export const vcConfirm = Confirm.function
 export const vcForm = Form
 export const vcFormItem = FormItem
 export const vcIcon = Icon
@@ -83,16 +83,16 @@ export const vcInputNumber = InputNumber
 export const vcLayer = Layer
 export const vcMessage = Message.function
 export const vcNotification = Notification.function
-export const vcOperationList = OperationList
 export const vcPagination = Pagination
-export const vcPopover = Popover
 export const vcProgress = Progress
+export const vcOperationList = OperationList
+export const vcPopover = Popover
 export const vcRadio = Radio
 export const vcRadioGroup = RadioGroup
+export const vcSelect = Select
 export const vcSwitch = Switch
 export const vcTabPane = TabPane
 export const vcTable = Table
-export const vcSelect = Select
 export const vcTabs = Tabs
 export const vcTimePicker = TimePicker
 
@@ -101,10 +101,10 @@ export default {
   install,
   Button,
   Collapse,
-  CollapseItem,
   ButtonGroup,
-  Confirm,
+  CollapseItem,
   ColorPicker,
+  Confirm,
   Form,
   FormItem,
   Icon,
@@ -113,16 +113,16 @@ export default {
   Layer,
   Message,
   Notification,
-  OperationList,
   Pagination,
-  Popover,
   Progress,
+  OperationList,
+  Popover,
   Radio,
   RadioGroup,
+  Select,
   Switch,
   TabPane,
   Table,
-  Select,
   Tabs,
   TimePicker
 }
