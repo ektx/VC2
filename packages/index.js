b61d1ceb2589c8f7d92aa5150bac5da1bb318a52
ektx <530675800@qq.com>
2020-12-11 11:06:34 +0800

    1.10.1

--- packages/index.js
+++ packages/index.js
@@ -1,20 +1,20 @@
-/* 以下代码自动生成于 Fri Dec 11 2020 10:49:56 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Fri Dec 11 2020 11:06:31 GMT+0800 (中国标准时间) */
 
 import Button from './components/button/index.vue'
 import ButtonGroup from './components/buttonGroup/index.vue'
-import CollapseItem from './components/collapseItem/index.vue'
 import Collapse from './components/collapse/index.vue'
-import Confirm from './components/confirm/index.js'
-import Form from './components/form/index.vue'
+import CollapseItem from './components/collapseItem/index.vue'
 import ColorPicker from './components/colorPicker/index.vue'
+import Form from './components/form/index.vue'
+import Confirm from './components/confirm/index.js'
 import FormItem from './components/formItem/index.vue'
 import Icon from './components/icon/index.vue'
 import Input from './components/input/index.vue'
 import InputNumber from './components/inputNumber/index.vue'
 import Layer from './components/layer/index.vue'
+import Message from './components/message/index.js'
 import Notification from './components/notification/index.js'
 import OperationList from './components/operationList/index.vue'
-import Message from './components/message/index.js'
 import Pagination from './components/pagination/index.vue'
 import Popover from './components/popover/index.vue'
 import Progress from './components/progress/index.vue'
@@ -22,8 +22,8 @@ import Radio from './components/radio/index.vue'
 import RadioGroup from './components/radioGroup/index.vue'
 import Select from './components/select/index.vue'
 import Switch from './components/switch/index.vue'
-import Table from './components/table/index.vue'
 import TabPane from './components/tabPane/index.vue'
+import Table from './components/table/index.vue'
 import Tabs from './components/tabs/index.vue'
 import TimePicker from './components/timePicker/index.vue'
 import Upload from './components/upload/index.vue'
@@ -31,19 +31,19 @@ import Upload from './components/upload/index.vue'
 const components = [
   Button,
   ButtonGroup,
-  CollapseItem,
   Collapse,
-  Confirm,
-  Form,
+  CollapseItem,
   ColorPicker,
+  Form,
+  Confirm,
   FormItem,
   Icon,
   Input,
   InputNumber,
   Layer,
+  Message,
   Notification,
   OperationList,
-  Message,
   Pagination,
   Popover,
   Progress,
@@ -51,8 +51,8 @@ const components = [
   RadioGroup,
   Select,
   Switch,
-  Table,
   TabPane,
+  Table,
   Tabs,
   TimePicker,
   Upload
@@ -73,19 +73,19 @@ const install = app => {
 
 export const vcButton = Button
 export const vcButtonGroup = ButtonGroup
-export const vcCollapseItem = CollapseItem
 export const vcCollapse = Collapse
-export const vcConfirm = Confirm.function
-export const vcForm = Form
+export const vcCollapseItem = CollapseItem
 export const vcColorPicker = ColorPicker
+export const vcForm = Form
+export const vcConfirm = Confirm.function
 export const vcFormItem = FormItem
 export const vcIcon = Icon
 export const vcInput = Input
 export const vcInputNumber = InputNumber
 export const vcLayer = Layer
+export const vcMessage = Message.function
 export const vcNotification = Notification.function
 export const vcOperationList = OperationList
-export const vcMessage = Message.function
 export const vcPagination = Pagination
 export const vcPopover = Popover
 export const vcProgress = Progress
@@ -93,30 +93,30 @@ export const vcRadio = Radio
 export const vcRadioGroup = RadioGroup
 export const vcSelect = Select
 export const vcSwitch = Switch
-export const vcTable = Table
 export const vcTabPane = TabPane
+export const vcTable = Table
 export const vcTabs = Tabs
 export const vcTimePicker = TimePicker
 export const vcUpload = Upload
 
 export default {
-  version: "1.10.0",
+  version: "1.10.1",
   install,
   Button,
   ButtonGroup,
-  CollapseItem,
   Collapse,
-  Confirm,
-  Form,
+  CollapseItem,
   ColorPicker,
+  Form,
+  Confirm,
   FormItem,
   Icon,
   Input,
   InputNumber,
   Layer,
+  Message,
   Notification,
   OperationList,
-  Message,
   Pagination,
   Popover,
   Progress,
@@ -124,8 +124,8 @@ export default {
   RadioGroup,
   Select,
   Switch,
-  Table,
   TabPane,
+  Table,
   Tabs,
   TimePicker,
   Upload
