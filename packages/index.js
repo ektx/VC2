9c0d6e1e62146edba6734cd2bcb9c7e7dc8acd02
ektx <530675800@qq.com>
2020-12-23 13:43:17 +0800

    1.10.3

--- packages/index.js
+++ packages/index.js
@@ -1,59 +1,59 @@
-/* 以下代码自动生成于 Wed Dec 23 2020 10:24:37 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Wed Dec 23 2020 13:43:15 GMT+0800 (中国标准时间) */
 
 import Button from './components/button/index.vue'
 import ButtonGroup from './components/buttonGroup/index.vue'
-import Collapse from './components/collapse/index.vue'
 import CollapseItem from './components/collapseItem/index.vue'
-import Confirm from './components/confirm/index.js'
+import Collapse from './components/collapse/index.vue'
 import ColorPicker from './components/colorPicker/index.vue'
 import Form from './components/form/index.vue'
+import Confirm from './components/confirm/index.js'
 import FormItem from './components/formItem/index.vue'
-import InputNumber from './components/inputNumber/index.vue'
-import Input from './components/input/index.vue'
 import Icon from './components/icon/index.vue'
-import Message from './components/message/index.js'
+import Input from './components/input/index.vue'
+import InputNumber from './components/inputNumber/index.vue'
 import Layer from './components/layer/index.vue'
 import Notification from './components/notification/index.js'
-import OperationList from './components/operationList/index.vue'
+import Message from './components/message/index.js'
 import Pagination from './components/pagination/index.vue'
-import Popover from './components/popover/index.vue'
-import RadioGroup from './components/radioGroup/index.vue'
+import OperationList from './components/operationList/index.vue'
 import Progress from './components/progress/index.vue'
+import Popover from './components/popover/index.vue'
 import Radio from './components/radio/index.vue'
+import RadioGroup from './components/radioGroup/index.vue'
 import Select from './components/select/index.vue'
 import Switch from './components/switch/index.vue'
 import TabPane from './components/tabPane/index.vue'
-import Table from './components/table/index.vue'
 import Tabs from './components/tabs/index.vue'
+import Table from './components/table/index.vue'
 import TimePicker from './components/timePicker/index.vue'
 import Upload from './components/upload/index.vue'
 
 const components = [
   Button,
   ButtonGroup,
-  Collapse,
   CollapseItem,
-  Confirm,
+  Collapse,
   ColorPicker,
   Form,
+  Confirm,
   FormItem,
-  InputNumber,
-  Input,
   Icon,
-  Message,
+  Input,
+  InputNumber,
   Layer,
   Notification,
-  OperationList,
+  Message,
   Pagination,
-  Popover,
-  RadioGroup,
+  OperationList,
   Progress,
+  Popover,
   Radio,
+  RadioGroup,
   Select,
   Switch,
   TabPane,
-  Table,
   Tabs,
+  Table,
   TimePicker,
   Upload
 ]
@@ -73,60 +73,60 @@ const install = app => {
 
 export const vcButton = Button
 export const vcButtonGroup = ButtonGroup
-export const vcCollapse = Collapse
 export const vcCollapseItem = CollapseItem
-export const vcConfirm = Confirm.function
+export const vcCollapse = Collapse
 export const vcColorPicker = ColorPicker
 export const vcForm = Form
+export const vcConfirm = Confirm.function
 export const vcFormItem = FormItem
-export const vcInputNumber = InputNumber
-export const vcInput = Input
 export const vcIcon = Icon
-export const vcMessage = Message.function
+export const vcInput = Input
+export const vcInputNumber = InputNumber
 export const vcLayer = Layer
 export const vcNotification = Notification.function
-export const vcOperationList = OperationList
+export const vcMessage = Message.function
 export const vcPagination = Pagination
-export const vcPopover = Popover
-export const vcRadioGroup = RadioGroup
+export const vcOperationList = OperationList
 export const vcProgress = Progress
+export const vcPopover = Popover
 export const vcRadio = Radio
+export const vcRadioGroup = RadioGroup
 export const vcSelect = Select
 export const vcSwitch = Switch
 export const vcTabPane = TabPane
-export const vcTable = Table
 export const vcTabs = Tabs
+export const vcTable = Table
 export const vcTimePicker = TimePicker
 export const vcUpload = Upload
 
 export default {
-  version: "1.10.2",
+  version: "1.10.3",
   install,
   Button,
   ButtonGroup,
-  Collapse,
   CollapseItem,
-  Confirm,
+  Collapse,
   ColorPicker,
   Form,
+  Confirm,
   FormItem,
-  InputNumber,
-  Input,
   Icon,
-  Message,
+  Input,
+  InputNumber,
   Layer,
   Notification,
-  OperationList,
+  Message,
   Pagination,
-  Popover,
-  RadioGroup,
+  OperationList,
   Progress,
+  Popover,
   Radio,
+  RadioGroup,
   Select,
   Switch,
   TabPane,
-  Table,
   Tabs,
+  Table,
   TimePicker,
   Upload
 }
