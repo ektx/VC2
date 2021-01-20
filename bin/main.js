08ec8f94fd88223e2c6c1de278d5a4733c0b7f33
ektx <530675800@qq.com>
2021-01-20 11:09:06 +0800

    chore:依赖调整

--- bin/main.js
+++ bin/main.js
@@ -1,47 +1,8 @@
 const { createServer } = require('vite')
 const express = require('express')
 const bodyParser = require('body-parser')
-const chalk = require('chalk')
 const vue = require('@vitejs/plugin-vue')
 const router = require('./router')
-
-const myPlugin = ({
-  root, // 项目目录的绝对地址
-  app, // Koa app
-  server, // raw http server instance
-  watcher, // chokidar file watcher instance
-  resolver
-}) => {
-  app
-    .use(router.routes())
-    .use(async (ctx, next) => {
-
-      // ...wait for vite to do built-in transforms
-      await next()
-
-      // Post processing before the content is served. Note this includes parts
-      // compiled from `*.vue` files, where <template> and <script> are served as
-      // `application/javascript` and <style> are served as `text/css`.
-      if (ctx.response.is('js')) {
-        console.log(chalk.green(`[JS] `) + ctx.url)
-      }
-
-    })
-    
-  watcher.on('change', file => {
-    if (file.endsWith('.md')) {
-      const path = resolver.fileToRequest(file)
-
-      watcher.send({
-        type: 'markdown-reload',
-        file,
-        path
-      })
-
-      console.log(chalk.green(`[MD] `) + path)
-    }
-  })
-}
 
 ;(async () => {
   const app = express()
