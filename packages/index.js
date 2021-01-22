e6e99d58b95c751955f2835ca0b5cbddb5881f0e
ektx <530675800@qq.com>
2021-01-22 08:41:16 +0800

    1.10.7

--- packages/index.js
+++ packages/index.js
@@ -1,32 +1,32 @@
-/* 以下代码自动生成于 Fri Jan 15 2021 10:12:58 GMT+0800 (中国标准时间) */
+/* 以下代码自动生成于 Fri Jan 22 2021 08:41:14 GMT+0800 (中国标准时间) */
 
 import ButtonGroup from './components/buttonGroup/index.vue'
 import Button from './components/button/index.vue'
 import Collapse from './components/collapse/index.vue'
 import CollapseItem from './components/collapseItem/index.vue'
 import ColorPicker from './components/colorPicker/index.vue'
-import Form from './components/form/index.vue'
 import Confirm from './components/confirm/index.js'
+import Form from './components/form/index.vue'
 import FormItem from './components/formItem/index.vue'
 import Icon from './components/icon/index.vue'
+import Layer from './components/layer/index.vue'
 import Input from './components/input/index.vue'
 import InputNumber from './components/inputNumber/index.vue'
-import Layer from './components/layer/index.vue'
-import Message from './components/message/index.js'
 import Notification from './components/notification/index.js'
+import Message from './components/message/index.js'
 import OperationList from './components/operationList/index.vue'
 import Pagination from './components/pagination/index.vue'
 import Popover from './components/popover/index.vue'
-import Radio from './components/radio/index.vue'
 import Progress from './components/progress/index.vue'
-import Select from './components/select/index.vue'
+import Radio from './components/radio/index.vue'
 import RadioGroup from './components/radioGroup/index.vue'
-import Switch from './components/switch/index.vue'
+import Select from './components/select/index.vue'
 import TabPane from './components/tabPane/index.vue'
-import Table from './components/table/index.vue'
+import Switch from './components/switch/index.vue'
 import Tabs from './components/tabs/index.vue'
-import TimePicker from './components/timePicker/index.vue'
+import Table from './components/table/index.vue'
 import Upload from './components/upload/index.vue'
+import TimePicker from './components/timePicker/index.vue'
 
 const components = [
   ButtonGroup,
@@ -34,28 +34,28 @@ const components = [
   Collapse,
   CollapseItem,
   ColorPicker,
-  Form,
   Confirm,
+  Form,
   FormItem,
   Icon,
+  Layer,
   Input,
   InputNumber,
-  Layer,
-  Message,
   Notification,
+  Message,
   OperationList,
   Pagination,
   Popover,
-  Radio,
   Progress,
-  Select,
+  Radio,
   RadioGroup,
-  Switch,
+  Select,
   TabPane,
-  Table,
+  Switch,
   Tabs,
-  TimePicker,
-  Upload
+  Table,
+  Upload,
+  TimePicker
 ]
 
 const install = app => {
@@ -76,57 +76,57 @@ export const vcButton = Button
 export const vcCollapse = Collapse
 export const vcCollapseItem = CollapseItem
 export const vcColorPicker = ColorPicker
-export const vcForm = Form
 export const vcConfirm = Confirm.function
+export const vcForm = Form
 export const vcFormItem = FormItem
 export const vcIcon = Icon
+export const vcLayer = Layer
 export const vcInput = Input
 export const vcInputNumber = InputNumber
-export const vcLayer = Layer
-export const vcMessage = Message.function
 export const vcNotification = Notification.function
+export const vcMessage = Message.function
 export const vcOperationList = OperationList
 export const vcPagination = Pagination
 export const vcPopover = Popover
-export const vcRadio = Radio
 export const vcProgress = Progress
-export const vcSelect = Select
+export const vcRadio = Radio
 export const vcRadioGroup = RadioGroup
-export const vcSwitch = Switch
+export const vcSelect = Select
 export const vcTabPane = TabPane
-export const vcTable = Table
+export const vcSwitch = Switch
 export const vcTabs = Tabs
-export const vcTimePicker = TimePicker
+export const vcTable = Table
 export const vcUpload = Upload
+export const vcTimePicker = TimePicker
 
 export default {
-  version: "1.10.6",
+  version: "1.10.7",
   install,
   ButtonGroup,
   Button,
   Collapse,
   CollapseItem,
   ColorPicker,
-  Form,
   Confirm,
+  Form,
   FormItem,
   Icon,
+  Layer,
   Input,
   InputNumber,
-  Layer,
-  Message,
   Notification,
+  Message,
   OperationList,
   Pagination,
   Popover,
-  Radio,
   Progress,
-  Select,
+  Radio,
   RadioGroup,
-  Switch,
+  Select,
   TabPane,
-  Table,
+  Switch,
   Tabs,
-  TimePicker,
-  Upload
+  Table,
+  Upload,
+  TimePicker
 }
