1465ede2e702d00e4d168148112ac1c2def17049
wenlong <530675800@qq.com>
2021-11-03 15:20:06 +0800

    样式分离

--- .vipress/src/components/codeBox/index.vue
+++ .vipress/src/components/codeBox/index.vue
@@ -9,7 +9,7 @@
     <div class="code-source">
       <div 
         class="code-source--main" 
-        :style="{ height }"
+        :style="style"
         @transitionend="transitionend"
       >
         <slot />
@@ -31,20 +31,24 @@ export default {
     return {
       open: false,
       intersectionObserver: null,
-      height: 0,
+      style: { height: 0 },
       isFullScreen: false
     }
   },
   watch: {
     open(val) {
-      let H = this.$el.querySelector('.code-source--main').scrollHeight
-
-      this.height = H + 'px'
-
-      if (!val) {
-        requestAnimationFrame(() => {
-          this.height = 0
-        })
+      if (this.isFullScreen) {
+        this.style = { '--height': val ? '50vh' : 0 }
+      } else {
+        let H = this.$el.querySelector('.code-source--main').scrollHeight
+  
+        this.style.height = H + 'px'
+  
+        if (!val) {
+          requestAnimationFrame(() => {
+            this.style.height = 0
+          })
+        }
       }
     },
   },
@@ -53,7 +57,7 @@ export default {
   },
   methods: {
     transitionend() {
-      this.open && (this.height = 'auto')
+      this.open && (this.style.height = '')
     },
     setSticky() {
       let footer = this.$el.querySelector('.code-source--footer')
@@ -76,8 +80,10 @@ export default {
 
       if (!document.fullscreenElement) {
         this.$el.requestFullscreen()
+        this.style = this.open ? {'--height': '50vh'} : this.style
       } else {
         document.exitFullscreen()
+        this.style = this.open ? {} : { height: 0 }
       }
       this.isFullScreen = !document.fullscreenElement
     }
@@ -90,121 +96,4 @@ export default {
 }
 </script>
 
-<style lang="less">
-.code-box {
-  margin: 1em 0;
-  border-radius: 10px;
-  border: 1px solid var(--codebox-border);
-  background-color: var(--codebox-bg);
-  will-change: background-color, box-shadow;
-  // transition: 
-  //   background .3s ease-out,
-  //   border-color .3s ease-in-out, 
-  //   box-shadow .3s ease-out;
-  transition: all .3s ease-in-out;
-
-  &:hover {
-    box-shadow: 2px 2px 15px var(--codebox-hover-shadow);
-  }
-
-  .code-display {
-    padding: 10px;
-  }
-
-  .code-source {
-    border-radius: 0 0 10px 10px;
-    border-top: 1px solid transparent;
-    transition: border-color .3s ease-in-out;
-
-    &.is-open {
-      border-top-color: var(--codebox-border);
-    }
-
-    &--main {
-      overflow: hidden;
-      transition: height .3s ease-in-out;
-      background-color: var(--page-bg-color);
-
-      & pre[class*='language-'] {
-        margin: 0;
-        background-color: transparent;
-      }
-
-      blockquote {
-        margin: 1em 1em 0;
-      }
-    }
-
-    &--footer {
-      position: sticky;
-      bottom: 0;
-      left: 0;
-      display: flex;
-      flex-direction: row-reverse;
-      align-items: center;
-      margin: 0;
-      padding: 0;
-      height: 30px;
-      list-style: none;
-      font-size: 12px;
-      color: var(--page-info-color);
-      border-top: 1px solid var(--codebox-border);
-      background-color: var(--codebox-footer-bg);
-      backdrop-filter: blur(5px);
-      box-sizing: border-box;
-      cursor: pointer;
-
-      li {
-        padding: 0 10px;
-        transition: color 0.3s ease-in-out;
-
-        &:hover {
-          color: var(--page-hold-color);
-        }
-      }
-    }
-  }
-  
-  &.is-full-screen {
-    display: flex;
-    flex-direction: column;
-    width: 100vw;
-    height: 100vh;
-    border-radius: 0;
-    position: relative;
-
-    .code-display {
-      flex: 1;
-      overflow: auto;
-      word-break: break-word;
-      transition: height .3s ease-in-out;
-    }
-
-    .code-source {
-      // position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      max-height: 70vh;
-      height: 30px;
-      // overflow: auto;
-      transition: height .3s ease-in-out;
-
-      &--main {
-        height: 100%;
-        overflow: auto;
-      }
-    }
-
-    &.is-open {
-      .code-display {
-        height: 30vh;
-      }
-
-      .code-source {
-        height: 70vh;
-      }
-    }
-  }
-}
-</style>
+<style lang="less" src="./style.less"></style>
