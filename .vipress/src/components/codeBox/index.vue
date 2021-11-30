a62b9c1b3dd15592a8b56d442dc95440483b35a1
wenlong <530675800@qq.com>
2021-11-30 09:42:42 +0800

    :art: 更新模板

--- .vipress/src/components/codeBox/index.vue
+++ .vipress/src/components/codeBox/index.vue
@@ -1,22 +1,19 @@
 <template>
-  <div :class="['code-box', { 'is-open': open }]">
+  <div class="code-box">
     <div class="code-display">
       <slot name="child" />
     </div>
     <div :class="['code-source', { 'is-open': open }]">
-      <div
-        class="code-source--main"
-        :style="sourceStyle"
+      <div 
+        class="code-source--main" 
+        :style="{ height }"
         @transitionend="transitionend"
       >
         <slot />
       </div>
-      <ul class="code-source--footer" @click="open = !open">
-        <li>{{ open ? '收起' : '查看' }}</li>
-        <li @click.stop="toggleScreen">
-          {{ isFullScreen ? '退出' : '全屏' }}
-        </li>
-      </ul>
+      <div class="code-source--footer" @click="open = !open">
+        {{ open ? '收起' : '查看' }}
+      </div>
       <div class="code-source--sentinel"></div>
     </div>
   </div>
@@ -47,9 +44,6 @@ export default {
   },
   mounted() {
     this.setSticky()
-    window.addEventListener('mousemove', this.resizeScreen)
-    window.addEventListener('mouseup', this.clearHoldBar)
-    this.$el.addEventListener('fullscreenchange', this.winResize)
   },
   methods: {
     transitionend() {
@@ -71,8 +65,9 @@ export default {
 
       this.intersectionObserver.observe(sentinel)
     },
-    toggleScreen() {
-      if (!document.fullscreenEnabled) return
+  },
+  beforeUnmount() {
+    let sentinel = this.$el.querySelector('.code-source--sentinel')
 
     this.intersectionObserver.unobserve(sentinel)
   },
@@ -86,10 +81,10 @@ export default {
   border: 1px solid var(--codebox-border);
   background-color: var(--codebox-bg);
   will-change: background, box-shadow;
-  transition:
+  transition: 
     background .3s ease-out,
-    border-color 0.3s ease-in-out,
-    box-shadow 0.3s ease-out;
+    border-color .3s ease-in-out, 
+    box-shadow .3s ease-out;
 
   &:hover {
     box-shadow: 2px 2px 15px var(--codebox-hover-shadow);
@@ -104,46 +99,28 @@ export default {
     border-top: 1px solid transparent;
     transition: border-color .3s ease-in-out;
 
-      if (this.clickCount >= 2) {
-        this.style = { '--height': '50vh' }
-      }
-    },
-    resizeScreen(evt) {
-      if (this.isHolderBar && this.isFullScreen) {
-        let h = evt.clientY / window.innerHeight
-        let footerH = 33 / window.innerHeight
+    &.is-open {
+      border-top-color: var(--codebox-border);
+    }
 
     &--main {
       overflow: hidden;
       transition: height .3s ease-in-out;
       background-color: var(--page-bg-color);
 
-        if (h > 0) {
-          h = h > 90 ? 90 : h
-          this.open = true
-        } else {
-          h = 0
-          this.open = false
-        }
-
-        this.varHeight = h + 'vh'
-
-        this.style = {
-          '--height': this.varHeight
-        }
-        document.documentElement.style.cursor = 'n-resize'
+      & pre[class*='language-'] {
+        margin: 0;
+        background-color: transparent;
       }
-    },
-    winResize(evt) {
-      this.isFullScreen = document.fullscreenElement === evt.target
-    },
-    clearHoldBar() {
-      this.isHolderBar = false
-      document.documentElement.style = ''
+
+      blockquote {
+        margin: 1em 1em 0;
+      }
     }
-  },
-  beforeUnmount() {
-    let sentinel = this.$el.querySelector('.code-source--sentinel')
+    &--footer {
+      position: sticky;
+      bottom: 0;
+      left: 0;
 
       font-size: 12px;
       color: #666;
@@ -153,13 +130,10 @@ export default {
       background-color: var(--codebox-footer-bg);
       backdrop-filter: blur(5px);
       cursor: pointer;
-      transition:
-        color 0.3s ease-in-out,
+      transition: 
+        color 0.3s ease-in-out, 
         background-color 0.3s ease-in-out;
     }
   }
 }
-</script>
-
-<style lang="less" src="./style.less">
 </style>
