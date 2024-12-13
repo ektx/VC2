b47a8decca5fa91d7f3f889e92017759687700f7
wenlong <530675800@qq.com>
2024-12-13 13:51:52 +0800

    ci: 更新配制

--- .vipress/server.js
+++ .vipress/server.js
@@ -1,13 +1,13 @@
 export default {
   title: 'VC2', // 标题
-  lang: 'zh-CN', // 网页语言
-  // icon: '/favicon.ico', // icon
   template: 'vue3',
-  homeRoute: '/doc/welcome', // 首页路由地址
-  routerDir: '/doc', // 路径目录
+  homePage: 'welcome.md', // 首页路由地址
+  docDir: 'doc', // 文档路由
   build: {
     // 打包设置
-    base: '/', // 公共基础路径
-    outDir: 'dist' // 指定输出目录，默认 dist
+    doc: {
+      base: '/', // 公共基础路径
+      outDir: 'dist' // 指定输出目录，默认 dist
+    }
   }
 }
