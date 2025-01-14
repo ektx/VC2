eaf7d4218db8a99711085b9180f87dbe3f932869
wenlong <530675800@qq.com>
2025-01-14 11:20:39 +0800

    update docs

--- .vipress/server.js
+++ .vipress/server.js
@@ -1,13 +1,12 @@
 export default {
   title: 'VC2', // 标题
   template: 'vue3',
-  homePage: 'welcome.md', // 首页路由地址
   docDir: 'doc', // 文档路由
+  homePage: 'welcome.md', // 首页路由地址
   build: {
     // 打包设置
     doc: {
-      base: '/', // 公共基础路径
-      outDir: 'docs' // 指定输出目录，默认 dist
+      base: '/VC2' // 公共基础路径
     },
     lib: {
       outDir: 'lib'
